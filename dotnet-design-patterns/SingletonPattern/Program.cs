edfc40d9ec1c3fb97a0505c763a9d2172cb15930
MERT METİN <mertmetin1993@gmail.com>
2023-03-04 22:09:58 +0300

    Adding singleton pattern

---
+++ dotnet-design-patterns/SingletonPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using SingletonPattern;
+
+try
+{
+    var personel1 = Personel.PersonelInstance;
+    var personel2 = Personel.PersonelInstance;
+
+    Console.WriteLine("Sayı 1:{0} Sayı 2:{1}", personel1.Number, personel2.Number);
+
+    if (personel1 == personel2)
+        Console.WriteLine("Nesneler aynıdır.");
+
+    else
+        Console.WriteLine("Nesneler farklıdır.");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.GetType());
+}
+finally
+{
+    Console.ReadLine();
+}
