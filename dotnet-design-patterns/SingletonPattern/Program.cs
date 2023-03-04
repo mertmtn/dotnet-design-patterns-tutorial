a5226e685cf457ef19834be95e3cd84287c58f05
MERT METİN <mertmetin1993@gmail.com>
2023-03-04 22:11:04 +0300

    Changing main program for singleton pattern

--- dotnet-design-patterns/SingletonPattern/Program.cs
+++ dotnet-design-patterns/SingletonPattern/Program.cs
@@ -1,23 +1,9 @@
 ﻿using SingletonPattern;
 
-try
-{
-    var personel1 = Personel.PersonelInstance;
-    var personel2 = Personel.PersonelInstance;
+var personel1 = Personel.PersonelInstance;
+var personel2 = Personel.PersonelInstance;
 
-    Console.WriteLine("Sayı 1:{0} Sayı 2:{1}", personel1.Number, personel2.Number);
-
-    if (personel1 == personel2)
-        Console.WriteLine("Nesneler aynıdır.");
-
-    else
-        Console.WriteLine("Nesneler farklıdır.");
-}
-catch (Exception e)
-{
-    Console.WriteLine(e.GetType());
-}
-finally
-{
-    Console.ReadLine();
-}
+Console.WriteLine("Number 1:{0} Number 2:{1}", personel1.Number, personel2.Number);
+ 
+string resultMessage = (personel1 == personel2) ? "Objects are same." : "Objects are not same";
+Console.WriteLine(resultMessage);
