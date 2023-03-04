08d21c127760e065e9839c6227fdaad228b0cecb
MERT METİN <mertmetin1993@gmail.com>
2023-03-04 23:07:06 +0300

    Adding Prototype Pattern

---
+++ dotnet-design-patterns/PrototypePattern/Personel.cs
@@ -0,0 +1,33 @@
+﻿namespace PrototypePattern
+{
+    public class Personel : PersonelPrototype
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Number { get; set; }
+
+        public Personel(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+
+            var r = new Random();
+            Number = r.Next();
+        }
+
+        public override string ToString()
+        {
+            return GetHashCode().ToString();
+        } 
+
+        public override PersonelPrototype Clone()
+        {
+            return MemberwiseClone() as PersonelPrototype;
+        }
+    }
+
+    public abstract class PersonelPrototype
+    {
+        public abstract PersonelPrototype Clone();
+    }
+}
---
+++ dotnet-design-patterns/PrototypePattern/Program.cs
@@ -0,0 +1,11 @@
+﻿using PrototypePattern;
+
+var personel1 = new Personel("Mert", "Metin");
+var personel2 = (Personel)personel1.Clone();
+
+Console.WriteLine("Person 1 Name:{0} \nPerson 2 Name:{1}", personel1.Name, personel2.Name);
+
+Console.WriteLine("Object Hash 1:{0} Object Hash 2:{1}", personel1.ToString(), personel2.ToString());
+
+string resultMessage = (personel1.Equals(personel2)) ? "Objects are same." : "Objects are not same";
+Console.WriteLine(resultMessage);
