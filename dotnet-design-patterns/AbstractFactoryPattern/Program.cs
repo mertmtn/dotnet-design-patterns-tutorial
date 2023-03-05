219297988e2950db9a6eee0e50e14a119c0edede
MERT METİN <mertmetin1993@gmail.com>
2023-03-05 13:30:51 +0300

    Adding Abstract Factory Pattern

---
+++ dotnet-design-patterns/AbstractFactoryPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using AbstractFactoryPattern;
+
+Shape rectangle = ShapeFactory.GetShape(new RectangleFactory());
+((Rectangle)rectangle).Width = 10;
+((Rectangle)rectangle).Height = 5;
+
+Shape circle = ShapeFactory.GetShape(new CircleFactory());
+((Circle)circle).Radius = 4;
+
+Console.WriteLine("Rectangle area: " + rectangle.CalculateArea() + " and size: " + rectangle.CalculateSize());
+Console.WriteLine("Circle area: " + circle.CalculateArea() + " and size: " + circle.CalculateSize());
---
+++ dotnet-design-patterns/AbstractFactoryPattern/Shape.cs
@@ -0,0 +1,73 @@
+﻿
+namespace AbstractFactoryPattern
+{
+    public interface Shape
+    {
+        public double CalculateArea();
+        public double CalculateSize();
+    }
+
+    public class Rectangle : Shape
+    {
+        private double width;
+        private double height;
+
+        public double Width { get => width; set => width = value; }
+        public double Height { get => height; set => height = value; }
+
+        public double CalculateArea()
+        {
+            return Width * Height;
+        }
+
+        public double CalculateSize()
+        {
+            return 2 * (Width + Height);
+        } 
+    }
+
+    public class Circle : Shape
+    {
+        private double radius; 
+        public double Radius { get => radius; set => radius = value; }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public double CalculateSize()
+        {
+            return 2 * Math.PI * Radius;
+        } 
+    }
+
+    public interface ShapeAbstractFactory
+    {
+        public Shape CreateShape();
+    }
+    public class CircleFactory : ShapeAbstractFactory
+    {
+
+        public Shape CreateShape()
+        {
+            return new Circle();
+        }
+    }
+    public class RectangleFactory : ShapeAbstractFactory
+    { 
+        public Shape CreateShape()
+        {
+            return new Rectangle();
+        }
+    }
+
+    public class ShapeFactory
+    {
+        public static Shape GetShape(ShapeAbstractFactory factory)
+        {
+            return factory.CreateShape();
+        }
+    }
+
+}
